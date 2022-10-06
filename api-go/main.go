8e16b2b337564e8c63c2241e43481f816aa09492
Pedro Mello <pedro.mello@govbr.com.br>
2022-10-06 12:01:10 -0300

    add go api study project

---
+++ api-go/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type user struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+var users = []user{
+	{ID: "1", Name: "Fulano 1"},
+	{ID: "2", Name: "Fulano 2"},
+}
+
+func getUsers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/users", getUsers)
+
+	router.Run("localhost:8080")
+}
