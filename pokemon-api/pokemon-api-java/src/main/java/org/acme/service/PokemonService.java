56bb81eae4a6751b79d414a6c0fc574442333176
Pedro Mello <pedromellogomes@gmail.com.br>
2022-11-16 21:47:36 -0300

    add: pokemon service

--- pokemon-api/pokemon-api-java/src/main/java/org/acme/service/PokemonService.java
+++ pokemon-api/pokemon-api-java/src/main/java/org/acme/service/PokemonService.java
@@ -1,4 +1,22 @@
 package org.acme.service;
 
+import org.acme.entity.Pokemon;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class PokemonService {
+
+    public Pokemon findByIdOptionalOrThrow(Long id) {
+        return Pokemon
+                .findByIdOptional(id)
+                .map(Pokemon.class::cast)
+                .orElseThrow(RuntimeException::new);
+    }
+    public Pokemon findByIdentifier(String identifier) {
+        return Pokemon
+                .findByIdentifierOptional(identifier)
+                .orElseThrow(RuntimeException::new);
+    }
+
 }
